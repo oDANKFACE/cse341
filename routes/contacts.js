5bcfd058e42ee50b3a9686fbfdd01308e0edb967
oDANKFACE <darien.r.weller@gmail.com>
2023-06-29 19:05:11 -0600

    Completed Lesson 02 again

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,14 +4,5 @@ const { listContacts, getContact} = require('../controllers/contacts')
 
 router.get('/', listContacts)
 router.get('/:id', getContact)
-router.post('/', (req,res) => {
-    // create a new contact
-})
-router.patch('/:id', (req,res) => {
-    // update a specific contact
-})
-router.delete('/:id', (req,res) => {
-    // delete a specific contact
-})
 
 module.exports = router
